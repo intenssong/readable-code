01cbb75dad3e09b76dd0aadeea82d12acf40113e
intenssong <intenssong@gmail.com>
2024-10-02 11:17:00 +0900

    OCP 개방폐쇄원칙 -BoardIndexConverter 클래스로 입력받은 인덱스 변환 관련 책임 분리 -GameLevel 인터페이스로 확장에 열린 설계로 변경

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,11 +1,14 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.gamelevel.Advanced;
+import cleancode.minesweeper.tobe.gamelevel.Beginner;
+
 public class GameApplication {
 
 
 
     public static void main(String[] args) {
-        Minesweeper minesweeper = new Minesweeper();
+        Minesweeper minesweeper = new Minesweeper(new Advanced());
         minesweeper.run();
     }
 
