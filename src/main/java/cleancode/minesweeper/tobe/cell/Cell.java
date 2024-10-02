bc87f791bd95facec69832c1cd754d657c81a3b3
intenssong <intenssong@gmail.com>
2024-10-02 16:40:18 +0900

    상속과 조합

--- src/main/java/cleancode/minesweeper/tobe/cell/Cell.java
+++ src/main/java/cleancode/minesweeper/tobe/cell/Cell.java
@@ -1,35 +1,22 @@
 package cleancode.minesweeper.tobe.cell;
 
-public abstract class Cell {
-    protected static final String FLAG_SIGN = "⚑";
+public interface Cell {
+    String FLAG_SIGN = "⚑";
+    String UNCHECKED_SIGN = "□";
 
-    protected static final String UNCHECKED_SIGN = "□";
 
-    protected boolean isFlagged;
-    protected boolean isOpened;
-    // Cell이 가진 속성 : 근처 지뢰 숫자, 지뢰 여부
-    // Cell의 상태 : 깃발 유무, 열렸다/닫혔다, 사용자가 확인.
+    boolean isLandMine();
 
-    public abstract boolean isLandMine();
+    boolean hasLandMineCount();
 
-    public abstract boolean hasLandMineCount();
+    String getSign();
 
-    public abstract String getSign();
+    void flag();
 
-    public void flag() {
-        this.isFlagged = true;
-    }
+    void open();
 
-    public void open() {
-        this.isOpened = true;
-    }
+    boolean isChecked();
 
-    public boolean isChecked() {
-        return isFlagged || isOpened;
-    }
-
-    public boolean isOpened() {
-        return isOpened;
-    }
+    boolean isOpened();
 
 }
