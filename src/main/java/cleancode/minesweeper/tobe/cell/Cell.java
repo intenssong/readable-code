1f73dceaead1288677490d5cb53df82438df011c
intenssong <intenssong@gmail.com>
2024-10-02 14:15:26 +0900

    LSP 리스코프 치환 원칙 -Cell 클래스를 자식클래스을 만들어 분리

---
+++ src/main/java/cleancode/minesweeper/tobe/cell/Cell.java
@@ -0,0 +1,35 @@
+package cleancode.minesweeper.tobe.cell;
+
+public abstract class Cell {
+    protected static final String FLAG_SIGN = "⚑";
+
+    protected static final String UNCHECKED_SIGN = "□";
+
+    protected boolean isFlagged;
+    protected boolean isOpened;
+    // Cell이 가진 속성 : 근처 지뢰 숫자, 지뢰 여부
+    // Cell의 상태 : 깃발 유무, 열렸다/닫혔다, 사용자가 확인.
+
+    public abstract boolean isLandMine();
+
+    public abstract boolean hasLandMineCount();
+
+    public abstract String getSign();
+
+    public void flag() {
+        this.isFlagged = true;
+    }
+
+    public void open() {
+        this.isOpened = true;
+    }
+
+    public boolean isChecked() {
+        return isFlagged || isOpened;
+    }
+
+    public boolean isOpened() {
+        return isOpened;
+    }
+
+}
